bbb15315421eaf2175d310076546c6b213440270
chubinho <andrewchubinets07@yandex.ru>
2026-03-13 19:23:51 +0300

    update file_source.py

--- src/sources/file_source.py
+++ src/sources/file_source.py
@@ -1,8 +1,10 @@
+import logging
 from typing import Protocol
 from pathlib import Path
 from src.task import Task
 import json
 
+logger = logging.getLogger("task")
 
 class FileSource:
     """
@@ -16,6 +18,7 @@ class FileSource:
             file_path
         """
         self.path = Path(file_path)
+        logger.info(f"Загрузка задач из файла: {self.path}")
 
     def get_task(self) -> list[Task]:
         """
@@ -31,11 +34,14 @@ class FileSource:
         """
         try:
             if not self.path.exists():
+                logger.error(f"Данный файл не найден: {self.path}")
                 raise FileNotFoundError(
                     f"Файл с заданием не найден:{self.path}")
 
             with open(self.path, 'r', encoding='utf-8') as f:
                 data = json.load(f)
+
+            logger.debug(f"Успешно прочитан JSON: {len(data)} записей")
 
             tasks = []
             for task_data in data:
@@ -44,11 +50,17 @@ class FileSource:
                     payload=task_data['payload']
                 )
                 tasks.append(task)
-            return tasks
+            
         except json.JSONDecodeError as e:
+            logger.error(f"Неверная запись JSON в файле {self.path}: {e}")
             raise ValueError(
                 f"Неверная запись JSON в файле {self.path}: {e}")
         except KeyError as e:
-            raise KeyError((f"Обязательное поле пропущено: {e}"))
+            logger.error(f"Обязательное поле пропущено: {e}")
+            raise KeyError(f"Обязательное поле пропущено: {e}")
         except Exception as e:
+            logger.error(f"Ошибка: {self.path}")
             raise ValueError(f"Ошибка: {self.path}")
+
+        logger.info(f"Создано {len(tasks)} объектов Task из файла")   
+        return tasks
